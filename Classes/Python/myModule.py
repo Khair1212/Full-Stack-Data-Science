e697df5171f2a7e38c4404bf6bd43e074fe931af
Khair Ahammed <khairahmad6@gmail.com>
2022-09-21 02:31:39 +0600

    DATA SCIENCE FOLLOW UP

---
+++ Classes/Python/myModule.py
@@ -0,0 +1,10 @@
+data = {"name": "ineuron",
+"course": ['data sciecne', 'blockchain', 'drone', 'robotics'],
+"greeting": 'greeting from ineuron'
+}
+
+def get_course():
+	return data['course']
+
+def greeting():
+	return data['greeting']
